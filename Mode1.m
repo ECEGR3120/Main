be6390382ad37ca63ab66d8b75c785bf28c2c5c9
Rwasghar <unbrokenconstraint@gmail.com>
2015-03-16 15:24:55 -0700

    Final Changes

--- Mode1.m
+++ Mode1.m
@@ -73,7 +73,7 @@ end
 %% Drawing Main Board
 function draw()
     %% Parameters
-    BOARDGAME.RESOLUTION = [400 800];
+    BOARDGAME.RESOLUTION = [400 800]; 
     BOARDGAME.WINDOW_RES = [400 800];
         MainFigureInitPos = [100 100];
         MainFigureSize = BOARDGAME.WINDOW_RES([2 1]).*2;
@@ -89,38 +89,39 @@ for f = 1:c
 rectangle('Position',[RECT.COMMAND_DRAW(1,f) RECT.COMMAND_DRAW(2,f) RECT.COMMAND_DRAW(3,f) RECT.COMMAND_DRAW(4,f)],'FaceColor',rand(1,3),'EdgeColor',rand(1,3));
 if RECT.COMMAND_DRAW(5,f) == 1
     RECT.COMMAND_DRAW(5,f) = 1;
-    text(RECT.COMMAND_DRAW(1,f)+RECT.COMMAND_DRAW(3,f)/4,RECT.COMMAND_DRAW(2,f)+RECT.COMMAND_DRAW(4,f)/2,char('Jump ahead by','1 space'),'FontName','Verdana');
+    text(RECT.COMMAND_DRAW(1,f)+RECT.COMMAND_DRAW(3,f)/2,RECT.COMMAND_DRAW(2,f)+RECT.COMMAND_DRAW(4,f)/2,char('Jump ahead by','1 space'),'FontName','Verdana','FontSize',7,'HorizontalAlignment', 'center');
 elseif RECT.COMMAND_DRAW(5,f) == -1
     RECT.COMMAND_DRAW(5,f) = 0;
 elseif RECT.COMMAND_DRAW(5,f) == 2
     RECT.COMMAND_DRAW(5,f) = 2;
-    text(RECT.COMMAND_DRAW(1,f)+RECT.COMMAND_DRAW(3,f)/4,RECT.COMMAND_DRAW(2,f)+RECT.COMMAND_DRAW(4,f)/2,char('Jump ahead by','2 spaces'),'FontName','Verdana');
+    text(RECT.COMMAND_DRAW(1,f)+RECT.COMMAND_DRAW(3,f)/2,RECT.COMMAND_DRAW(2,f)+RECT.COMMAND_DRAW(4,f)/2,char('Jump by','2 spaces'),'FontName','Verdana','FontSize',7,'HorizontalAlignment', 'center');
 elseif RECT.COMMAND_DRAW(5,f) == 3
     RECT.COMMAND_DRAW(5,f) = 3;
-    text(RECT.COMMAND_DRAW(1,f)+RECT.COMMAND_DRAW(3,f)/4,RECT.COMMAND_DRAW(2,f)+RECT.COMMAND_DRAW(4,f)/2,char('Jump ahead by','3 spaces'),'FontName','Verdana');
+    text(RECT.COMMAND_DRAW(1,f)+RECT.COMMAND_DRAW(3,f)/2,RECT.COMMAND_DRAW(2,f)+RECT.COMMAND_DRAW(4,f)/2,char('Jump by','3 spaces'),'FontName','Verdana','FontSize',7,'HorizontalAlignment', 'center');
 elseif RECT.COMMAND_DRAW(5,f) == 4
     RECT.COMMAND_DRAW(5,f) = 4;
-    text(RECT.COMMAND_DRAW(1,f)+RECT.COMMAND_DRAW(3,f)/4,RECT.COMMAND_DRAW(2,f)+RECT.COMMAND_DRAW(4,f)/2,char('Jump ahead by','4 spaces'),'FontName','Verdana');
+    text(RECT.COMMAND_DRAW(1,f)+RECT.COMMAND_DRAW(3,f)/2,RECT.COMMAND_DRAW(2,f)+RECT.COMMAND_DRAW(4,f)/2,char('Jump by','4 spaces'),'FontName','Verdana','FontSize',7,'HorizontalAlignment', 'center');
 elseif RECT.COMMAND_DRAW(5,f) == 5
     RECT.COMMAND_DRAW(5,f) = 5;
-    text(RECT.COMMAND_DRAW(1,f)+RECT.COMMAND_DRAW(3,f)/4,RECT.COMMAND_DRAW(2,f)+RECT.COMMAND_DRAW(4,f)/2,char('Jump ahead by','5 spaces'),'FontName','Verdana');
+    text(RECT.COMMAND_DRAW(1,f)+RECT.COMMAND_DRAW(3,f)/2,RECT.COMMAND_DRAW(2,f)+RECT.COMMAND_DRAW(4,f)/2,char('Jump by','5 spaces'),'FontName','Verdana','FontSize',7,'HorizontalAlignment', 'center');
 elseif RECT.COMMAND_DRAW(5,f) == 6
     RECT.COMMAND_DRAW(5,f) = 6;
-    text(RECT.COMMAND_DRAW(1,f)+RECT.COMMAND_DRAW(3,f)/4,RECT.COMMAND_DRAW(2,f)+RECT.COMMAND_DRAW(4,f)/2,char('Jump ahead by','6 spaces'),'FontName','Verdana');
+    text(RECT.COMMAND_DRAW(1,f)+RECT.COMMAND_DRAW(3,f)/2,RECT.COMMAND_DRAW(2,f)+RECT.COMMAND_DRAW(4,f)/2,char('Jump by','6 spaces'),'FontName','Verdana','FontSize',7,'HorizontalAlignment', 'center');
 elseif RECT.COMMAND_DRAW(5,f) == 7
     RECT.COMMAND_DRAW(5,f) = 7;
-    text(RECT.COMMAND_DRAW(1,f)+RECT.COMMAND_DRAW(3,f)/4,RECT.COMMAND_DRAW(2,f)+RECT.COMMAND_DRAW(4,f)/2,char('Jump ahead by','7 spaces'),'FontName','Verdana');
+    text(RECT.COMMAND_DRAW(1,f)+RECT.COMMAND_DRAW(3,f)/2,RECT.COMMAND_DRAW(2,f)+RECT.COMMAND_DRAW(4,f)/2,char('Jump by','7 spaces'),'FontName','Verdana','FontSize',7,'HorizontalAlignment', 'center');
 elseif RECT.COMMAND_DRAW(5,f) == 8
     RECT.COMMAND_DRAW(5,f) = 8;
-    text(RECT.COMMAND_DRAW(1,f)+RECT.COMMAND_DRAW(3,f)/4,RECT.COMMAND_DRAW(2,f)+RECT.COMMAND_DRAW(4,f)/2,char('Jump ahead by','8 spaces'),'FontName','Verdana');
+    text(RECT.COMMAND_DRAW(1,f)+RECT.COMMAND_DRAW(3,f)/2,RECT.COMMAND_DRAW(2,f)+RECT.COMMAND_DRAW(4,f)/2,char('Jump by','8 spaces'),'FontName','Verdana','FontSize',7,'HorizontalAlignment', 'center');
 elseif RECT.COMMAND_DRAW(5,f) == 9
     RECT.COMMAND_DRAW(5,f) = 9;
-    text(RECT.COMMAND_DRAW(1,f)+RECT.COMMAND_DRAW(3,f)/4,RECT.COMMAND_DRAW(2,f)+RECT.COMMAND_DRAW(4,f)/2,char('Jump ahead by','9 spaces'),'FontName','Verdana');
+    text(RECT.COMMAND_DRAW(1,f)+RECT.COMMAND_DRAW(3,f)/2,RECT.COMMAND_DRAW(2,f)+RECT.COMMAND_DRAW(4,f)/2,char('Jump by','9 spaces'),'FontName','Verdana','FontSize',7,'HorizontalAlignment', 'center');
 elseif RECT.COMMAND_DRAW(5,f) == 10
     RECT.COMMAND_DRAW(5,f) = 10;
-    text(RECT.COMMAND_DRAW(1,f)+RECT.COMMAND_DRAW(3,f)/4,RECT.COMMAND_DRAW(2,f)+RECT.COMMAND_DRAW(4,f)/2,char('Jump ahead by','10 spaces'),'FontName','Verdana');
+    text(RECT.COMMAND_DRAW(1,f)+RECT.COMMAND_DRAW(3,f)/2,RECT.COMMAND_DRAW(2,f)+RECT.COMMAND_DRAW(4,f)/2,char('Jump by','10 spaces'),'FontName','Verdana','FontSize',7,'HorizontalAlignment', 'center');
 end
 end
+BOARDGAME.title = text(50,100,char('ECEGR1000: Final'),'FontName','Verdana','FontSize',20,'HorizontalAlignment', 'center','Visible','off');
 end
 
 
@@ -128,14 +129,20 @@ end
 function numberofplayers()
 [ro co] = size(BOARDGAME.DICEROLL);
 [ro_1 co_1] = size(RECT.COMMAND_DRAW);
-ScoreInfoHd1 = text(50,50, 'Current Player:','FontName','Arial','FontSize',20,'HorizontalAlignment','center','Color',[1 1 1],'Visible','off');
-im = imread('sprite.png');
-im2 = imread('sprite2.png');
+ScoreInfoHd1 = text(50,-10, 'Current Player:','FontName','Arial','FontSize',20,'HorizontalAlignment','center','Color',[0 0 0],'Visible','off');
+set(BOARDGAME.title,'Visible','on');
 t = 0;
+im = {};    
 for n = 1:co
-Player(n) = image(im,'x',[RECT.COMMAND_DRAW(1,1)+4 RECT.COMMAND_DRAW(1,1)+1],'y',[RECT.COMMAND_DRAW(2,1)+4 RECT.COMMAND_DRAW(2,1)+1],'Visible','on');
+im{n} = imread(sprintf('sprite%d.png',n));
+Player(n) = image(im{n},'x',[RECT.COMMAND_DRAW(1,1)+4 RECT.COMMAND_DRAW(1,1)+1],'y',[RECT.COMMAND_DRAW(2,1)+4+n RECT.COMMAND_DRAW(2,1)+1+n],'Visible','on');
 t = t +1 ;
 person.Current(n).pos = [RECT.COMMAND_DRAW(1,1) RECT.COMMAND_DRAW(2,1)] ;
+end
+
+for n = 1:co
+Playerlegend(n) = image(im{n},'x',[-12 -15],'y',[-12+6*n -15+6*n],'Visible','on');
+legendtext = text(-5, -14+6*n, sprintf('Player %d',n), 'FontSize', 12, 'Color',[0 0 0], 'HorizontalAlignment', 'right');
 end
 
 i = 1;
@@ -160,7 +167,7 @@ while iterator ~= 1
         break;
         end
     set(Player(n),'x',[RECT.COMMAND_DRAW(1,person.Current(n).pos(1))+5 RECT.COMMAND_DRAW(1,person.Current(n).pos(1))+2]...
-        ,'y',[RECT.COMMAND_DRAW(2,person.Current(n).pos(2))+4 RECT.COMMAND_DRAW(2,person.Current(n).pos(2))+1],'Visible','on');
+        ,'y',[RECT.COMMAND_DRAW(2,person.Current(n).pos(2))+4+n RECT.COMMAND_DRAW(2,person.Current(n).pos(2))+1+n],'Visible','on');
     pause(0.5);
     if RECT.COMMAND_DRAW(5,person.Current(n).pos(1)) ~= 0
             pause(1)
@@ -171,7 +178,7 @@ while iterator ~= 1
             person.Current(n).pos(2) = person.Current(n).pos(2)+RECT.COMMAND_DRAW(5,person.Current(n).pos(2));
             if person.Current(n).pos(1) >= co_1 || person.Current(n).pos(2) >= co_1
             pause(1);
-            set(Player(n),'x',[RECT.COMMAND_DRAW(1,end)+4 RECT.COMMAND_DRAW(1,end)+1]...
+            set(Player(n),'x',[RECT.COMMAND_DRAW(1,end)+5 RECT.COMMAND_DRAW(1,end)+2]...
             ,'y',[RECT.COMMAND_DRAW(2,end)+4 RECT.COMMAND_DRAW(2,end)+1],'Visible','on');
             pause(1);
             Winner = sprintf('Player %d wins!',n);
@@ -179,7 +186,7 @@ while iterator ~= 1
             break;
             end
             set(Player(n),'x',[RECT.COMMAND_DRAW(1,person.Current(n).pos(1))+5 RECT.COMMAND_DRAW(1,person.Current(n).pos(1))+2]...
-            ,'y',[RECT.COMMAND_DRAW(2,person.Current(n).pos(2))+4 RECT.COMMAND_DRAW(2,person.Current(n).pos(2))+1],'Visible','on');
+            ,'y',[RECT.COMMAND_DRAW(2,person.Current(n).pos(2))+4+n RECT.COMMAND_DRAW(2,person.Current(n).pos(2))+1+n],'Visible','on');
             
     end
     n = n + 1;
